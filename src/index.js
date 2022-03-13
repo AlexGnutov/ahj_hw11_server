51f059d78757010abc2743de29709cc893bca273
AlexGnutov <81751916+AlexGnutov@users.noreply.github.com>
2022-03-13 10:33:47 +0300

    initial commit 2

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ app.use(async (ctx, next) => {
     if (!origin) {
         return await next();
     }
-    const headers = { 'Access-Control-Allow-Origin': '*', };
+    const headers = { 'Access-Control-Allow-Origin': '*' };
     if (ctx.request.method !== 'OPTIONS') {
         ctx.response.set({...headers});
         try {
